d7aceca6e7166f5cd6d443ccd5e53380d8f32938
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-05-05 19:37:55 +0530

    middleware deleted and auth checked in profile/route.js

--- app/api/user/profile/route.js
+++ app/api/user/profile/route.js
@@ -1,22 +1,26 @@
 import { getDataFromToken } from "@/helpers/getDataFromToken";
-
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/useModel";
 import { connect } from "@/dbConfig/dbConfig";
 
 connect();
 
-export async function GET(request){
-
+export async function GET(request) {
     try {
         const userId = await getDataFromToken(request);
-        const user = await User.findOne({_id: userId}).select("-password");
+
+        // If token is missing or invalid, redirect to /login
+        if (!userId) {
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+
+        const user = await User.findOne({ _id: userId }).select("-password");
+
         return NextResponse.json({
             mesaaage: "User found",
-            data: user
-        })
+            data: user,
+        });
     } catch (error) {
-        return NextResponse.json({error: error.message}, {status: 400});
+        return NextResponse.json({ error: error.message }, { status: 400 });
     }
-
 }
